43636495570577ed9972272719097d1af20bf86c
Steven Fisher <steven.fisher22@gmail.com>
2019-04-15 16:31:00 -0500

    setup of basic app complete

--- src/index.js
+++ src/index.js
@@ -2,16 +2,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Redux
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+// Components
+import Reddit from './Reddit';
+
 // CSS
 import './index.css';
 
-const App = () => {
-    return (
-        <div>
-            Test
-        </div>
-    )
-}
+// Initial State
+const initialState = {
+    posts: []
+};
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+// Reducer
+function reducer(state = initialState, action) {
+    return state;
+};
+
+// Store
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider>
+        <Reddit />
+    </Provider>, 
+    document.querySelector('#root')
+);
 
