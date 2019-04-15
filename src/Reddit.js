2644f5598aa44ed7d494d8ea3422bdf6aed5d22d
Steven Fisher <steven.fisher22@gmail.com>
2019-04-15 16:20:22 -0500

    added Reddit component

---
+++ src/Reddit.js
@@ -0,0 +1,31 @@
+// React Elements
+import React from 'react';
+
+// Redux
+import { connect } from 'react-redux';
+
+class Reddit extends React.Component {
+    render() {
+        const { posts } = this.props;
+
+        return (
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>
+                        <span>{post.score}</span>
+                        <div>
+                            <h2>{post.title}</h2>
+                            <div>{post.num_comments} comments</div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+}
+
+const mapState = state => ({
+    posts: state.posts
+})
+
+export default connect(mapState)(Reddit)
